33800a3c11e180d87cb54d8c1e4cd01203722d57
Caitlin Cooling <ccooling97@gmail.com>
2019-02-05 17:09:06 +0000

    Updated unit tests to reflect read now formatting the logs

--- spec/units/log_file_spec.rb
+++ spec/units/log_file_spec.rb
@@ -1,12 +1,9 @@
 require 'log_file.rb'
 
 describe LogFile do
-  it 'Handles the log file given' do
+  it 'Reads the log file given and returns the url with a count' do
     file = LogFile.new('webserver.log')
-    expect(file.read).to include "/help_page/1 126.318.035.038\n"
-    expect(file.read).to include "/contact 184.123.665.067\n"
-    expect(file.read).to include "/home 184.123.665.067\n"
-    expect(file.read).to include "/about/2 444.701.448.104\n"
+    expect(file.read).to include ["/help_page/1", 80]
   end
 end
 
