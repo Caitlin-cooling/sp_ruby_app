a214f59d9513055a9c1760b3ae38d8fdec0a0518
Caitlin Cooling <ccooling97@gmail.com>
2019-02-05 16:21:44 +0000

    Refactor LogFile
    - Now opens the file and reads the contents before closing the file

--- lib/log_file.rb
+++ lib/log_file.rb
@@ -4,6 +4,8 @@ class LogFile
   end
 
   def read
-    return '/help_page/1 126.318.035.038 /contact 184.123.665.067 /home 184.123.665.067 /about/2 444.701.448.104'
+    file = File.open(@file, "r")
+    return file.readlines
+    file.close
   end
 end
--- spec/log_file_spec.rb
+++ spec/log_file_spec.rb
@@ -3,6 +3,9 @@ require 'log_file.rb'
 describe LogFile do
   it 'Handles the log file given' do
     file = LogFile.new('webserver.log')
-    expect(file.read).to include '/help_page/1 126.318.035.038 /contact 184.123.665.067 /home 184.123.665.067 /about/2 444.701.448.104'
+    expect(file.read).to include "/help_page/1 126.318.035.038\n"
+    expect(file.read).to include "/contact 184.123.665.067\n"
+    expect(file.read).to include "/home 184.123.665.067\n"
+    expect(file.read).to include "/about/2 444.701.448.104\n"
   end
 end
