042556f35eb02569b7ddb48357e26f7a6b3a12a1
Caitlin Cooling <ccooling97@gmail.com>
2019-02-06 09:05:04 +0000

    Refactor for readibility

--- spec/units/log_file_spec.rb
+++ spec/units/log_file_spec.rb
@@ -1,12 +1,12 @@
 require 'log_file.rb'
 
 describe LogFile do
+  let(:file) {LogFile.new('webserver.log')}
+
   it 'Reads the log file given and returns the url with a count' do
-    file = LogFile.new('webserver.log')
     expect(file.read).to include ["/help_page/1", 80]
   end
 end
-
 
 describe Counter do
   let(:array) {['/help_page/1 126.318.035.038', '/contact 184.123.665.067', '/contact 184.123.665.060', '/about/2 444.701.448.104']}
